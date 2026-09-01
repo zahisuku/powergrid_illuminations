66bded84c625acf8a2adfe0edf5893e4d71ee342
zahisuku <shangbianhengping@gmail.com>
2026-09-01 21:19:06 +0900

    architectury-1.21.1-fabric-neoforge templete

---
+++ common/src/main/java/com/github/zahisuku/powergrid_illuminations/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.github.zahisuku.powergrid_illuminations;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "powergrid_illuminations";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/github/zahisuku/powergrid_illuminations/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.github.zahisuku.powergrid_illuminations.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.github.zahisuku.powergrid_illuminations.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
