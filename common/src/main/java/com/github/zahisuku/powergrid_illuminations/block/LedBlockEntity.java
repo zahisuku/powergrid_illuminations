51b74a0548509b9ca9ef4d7c5223c5e573fe22fa
zahisuku <shangbianhengping@gmail.com>
2026-09-03 00:56:50 +0900

    STEP5: add LedBlockEntity skeleton

---
+++ common/src/main/java/com/github/zahisuku/powergrid_illuminations/block/LedBlockEntity.java
@@ -0,0 +1,12 @@
+package com.github.zahisuku.powergrid_illuminations.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.patryk3211.powergrid.electricity.base.ElectricBlockEntity;
+
+public class LedBlockEntity extends ElectricBlockEntity {
+    public LedBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+}
