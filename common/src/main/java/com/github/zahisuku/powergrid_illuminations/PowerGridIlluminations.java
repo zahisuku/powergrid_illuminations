11ce580338beea69f24f70af9249ab358c8c400d
zahisuku <shangbianhengping@gmail.com>
2026-09-02 20:01:42 +0900

    add - worldに入ると、メッセージを出力するようにしました。

--- common/src/main/java/com/github/zahisuku/powergrid_illuminations/PowerGridIlluminations.java
+++ common/src/main/java/com/github/zahisuku/powergrid_illuminations/PowerGridIlluminations.java
@@ -1,9 +1,18 @@
 package com.github.zahisuku.powergrid_illuminations;
 
+import dev.architectury.event.events.common.PlayerEvent;
+import net.minecraft.network.chat.Component;
+
+import com.github.zahisuku.powergrid_illuminations.registry.ModBlocks;
+
+
 public final class PowerGridIlluminations {
     public static final String MOD_ID = "powergrid_illuminations";
 
     public static void init() {
         // Write common init code here.
+        PlayerEvent.PLAYER_JOIN.register(player -> {
+            player.sendSystemMessage(Component.literal("Welcome to Power Grid Illuminations!"));
+        });
     }
 }
