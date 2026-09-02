a1245bde4b6632e42b27d07a01f170407035dd25
zahisuku <shangbianhengping@gmail.com>
2026-09-03 01:39:34 +0900

    Implement LED fixed 15 ohm circuit

--- common/src/main/java/com/github/zahisuku/powergrid_illuminations/block/LedBlockEntity.java
+++ common/src/main/java/com/github/zahisuku/powergrid_illuminations/block/LedBlockEntity.java
@@ -6,6 +6,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.patryk3211.powergrid.electricity.base.ElectricBlockEntity;
 
 public class LedBlockEntity extends ElectricBlockEntity {
+    private static final float LED_RESISTANCE = 15.0f;
+
+    private ElectricWire wire;
+
     public LedBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -13,5 +17,10 @@ public class LedBlockEntity extends ElectricBlockEntity {
     @Override
     public void buildCircuit(CircuitBuilder builder) {
         builder.setTerminalCount(2);
+        wire = builder.connect(
+            LED_RESISTANCE,
+            builder.terminalNode(0),
+            builder.terminalNode(1)
+        );
     }
 }
